4cc94143507bdc70731246d29418acfe3f212e84
Ludwig S <silvaludwig@outlook.com>
2025-04-21 08:26:26 -0300

    01-quiz.py | choosing theme for the quiz

--- 01-quiz-game/01-quiz.py
+++ 01-quiz-game/01-quiz.py
@@ -17,3 +17,44 @@ elif play_or_not in "Yy":
     print("Ok, let's go!")
 else:
     print("I don't know what u mean pal...")
+    quit()
+
+# Choosing the theme
+print("=" * 50)
+print("First, you need to choose the theme...")
+print(
+    """
+    1 - MUSIC AND ART
+    2 - CINEMA AND THEATER
+    3 - GEOPOLITICS AND RELATED
+    4 - TECH
+"""
+)
+print("=" * 50)
+
+theme = int(input("Wich one? "))
+print()
+
+
+if theme == 1:
+    print("You choose 1")
+    print("Let's talk about MUSIC AND ART!")
+
+elif theme == 2:
+    print("You choose 2")
+    print("Let's talk about CINEMA AND THEATER!")
+
+
+elif theme == 3:
+    print("You choose 3")
+    print("Let's talk about GEOPOLITICS AND RELATED!")
+
+
+elif theme == 4:
+    print("You choose 4")
+    print("Let's talk about TECH!")
+
+
+else:
+    print("Oops, I didn't understand")
+    quit()
