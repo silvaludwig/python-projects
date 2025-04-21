eabe76fa05685b35842165d79de716c8f499f67a
Ludwig S <silvaludwig@outlook.com>
2025-04-21 17:29:00 -0300

    making a loop so the user can play it as long as wanted

--- 01-quiz-game/main.py
+++ 01-quiz-game/main.py
@@ -18,46 +18,49 @@ else:
     print("I don't know what u mean pal...")
     quit()
 
+
 # Choosing the theme
-print("=" * 50)
-print("First, you need to choose the theme...")
-print(
+while True:
+    print("=" * 50)
+    print("First, you need to choose the theme...")
+    print(
+        """
+        1 - MUSIC AND ART
+        2 - CINEMA AND THEATER
+        3 - GEOPOLITICS AND RELATED
+        4 - TECH
     """
-    1 - MUSIC AND ART
-    2 - CINEMA AND THEATER
-    3 - GEOPOLITICS AND RELATED
-    4 - TECH
-"""
-)
-print("=" * 50)
+    )
+    print("=" * 50)
 
-theme = int(input("Wich one? "))
-print()
+    theme = int(input("Wich one? "))
+    print()
 
+    if theme == 1:
+        print("You choose 1")
+        print("Let's talk about MUSIC AND ART!")
+        easy_music_and_art()
 
-if theme == 1:
-    print("You choose 1")
-    print("Let's talk about MUSIC AND ART!")
-    easy_music_and_art()
+    elif theme == 2:
+        print("You choose 2")
+        print("Let's talk about CINEMA AND THEATER!")
+        easy_cinema()
 
-elif theme == 2:
-    print("You choose 2")
-    print("Let's talk about CINEMA AND THEATER!")
-    easy_cinema()
+    elif theme == 3:
+        print("You choose 3")
+        print("Let's talk about GEOPOLITICS AND RELATED!")
+        easy_geopolitics()
 
+    elif theme == 4:
+        print("You choose 4")
+        print("Let's talk about TECH!")
+        easy_tech()
 
-elif theme == 3:
-    print("You choose 3")
-    print("Let's talk about GEOPOLITICS AND RELATED!")
-    easy_geopolitics()
+    else:
+        print("Oops, I didn't understand")
+        quit()
 
-
-elif theme == 4:
-    print("You choose 4")
-    print("Let's talk about TECH!")
-    easy_tech()
-
-
-else:
-    print("Oops, I didn't understand")
-    quit()
+    play_again = input("Wanna play again? [Y or N] ")
+    if play_again not in "Yy":
+        print("Ok then, see ya!")
+        break
